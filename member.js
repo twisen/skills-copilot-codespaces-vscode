636bcdb4233723f6e4a4678de5ee186b60dabac2
Enrique Vázquez <quique_vzquez@yahoo.es>
2023-11-14 12:14:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateURL: 'https://hoisdf/hoidf',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: 'm'
+        }
+
+    };
+}
